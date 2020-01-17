9d4e62149c40262a263774020768edeef5170398
Alexander Kursikov <alexander.kursikov@venuiti.com>
2020-01-17 09:06:19 -0500

    Render the whole list of changes as plain html without using react-virtualized.

--- src/TreeDiffViewer/index.js
+++ src/TreeDiffViewer/index.js
@@ -13,7 +13,7 @@ import { styled } from '@material-ui/styles'
 
 import styles from './index.css'
 
-import { AutoSizer, List } from 'react-virtualized';
+// import { AutoSizer, List } from 'react-virtualized';
 import 'react-virtualized/styles.css'; // only needs to be imported once
 
 import { compare, updateDoses } from './logic';
@@ -22,75 +22,129 @@ const MyBox = styled(Box)({
   height: 'calc(100vh - 150px)',
 });
 
+// const TreeDiff = (props) => {
+//   const { past, current, options } = props
+
+//   if (options.baseKeyMap) {
+//     options.baseKeys = options.baseKeys.map((baseKey) => (options.baseKeyMap[baseKey] ? options.baseKeyMap[baseKey] : baseKey))
+//   }
+
+//   const diffString = compare(past, current, options);
+
+//   const getRowHeight = ({ index }) => {
+//     const characterCount = 72;
+//     return 30 * Math.ceil(Math.max(
+//       diffString.left[index].length / characterCount,
+//       diffString.right[index].length / characterCount
+//     ));
+//   }
+
+//   const rowRenderer = ({ key, index, style }) => {
+//     const newStyles = {
+//       marker: {
+//         width: '10px',
+//         textAlign: 'center',
+//         height: '30px'
+//       },
+//       content: {
+//         width: '400px'
+//       },
+//       diffContainer: {
+//         whiteSpace: 'nowrap',
+//         pre: {
+//           wordBreak: 'break-all',
+//           lineHeight: '30px'
+//         }
+//       },
+//       wordDiff: {
+//         paddingTop: 0,
+//         paddingBottom: 0,
+//         lineHeight: '30px',
+//         display: 'inline'
+//       }
+//     };
+//     return (
+//       <div key={key} style={style}>
+//         <ReactDiffViewer
+//           styles={newStyles}
+//           oldValue={diffString.left[index]}
+//           newValue={diffString.right[index]}
+//           splitView={true}
+//           compareMethod={DiffMethod.WORDS}
+//           hideLineNumbers={true}
+//           showDiffOnly={false}
+//         />
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <AutoSizer>
+//       {({ width, height }) => (
+//         <List
+//           className={styles.List}
+//           height={height}
+//           rowCount={diffString.left.length}
+//           rowHeight={getRowHeight}
+//           rowRenderer={rowRenderer}
+//           width={width}
+//         />
+//       )}
+//     </AutoSizer>
+//   )
+// }
+
 const TreeDiff = (props) => {
   const { past, current, options } = props
 
-  if (options.baseKeyMap) {
-    options.baseKeys = options.baseKeys.map((baseKey) => (options.baseKeyMap[baseKey] ? options.baseKeyMap[baseKey] : baseKey))
-  }
-
   const diffString = compare(past, current, options);
 
-  const getRowHeight = ({ index }) => {
-    const characterCount = 72;
-    return 30 * Math.ceil(Math.max(
-      diffString.left[index].length / characterCount,
-      diffString.right[index].length / characterCount
-    ));
-  }
 
-  const rowRenderer = ({ key, index, style }) => {
-    const newStyles = {
+  const rows = [];
+  const newStyles = {
       marker: {
-        width: '10px',
-        textAlign: 'center',
-        height: '30px'
+          width: '10px',
+          textAlign: 'center',
+          height: '30px'
       },
       content: {
-        width: '400px'
+          width: '400px'
       },
       diffContainer: {
-        whiteSpace: 'nowrap',
-        pre: {
-          wordBreak: 'break-all',
-          lineHeight: '30px'
-        }
+          whiteSpace: 'nowrap',
+          pre: {
+              wordBreak: 'break-all',
+              lineHeight: '30px'
+          }
       },
       wordDiff: {
-        paddingTop: 0,
-        paddingBottom: 0,
-        lineHeight: '30px',
-        display: 'inline'
+          paddingTop: 0,
+          paddingBottom: 0,
+          lineHeight: '30px',
+          display: 'inline'
       }
-    };
-    return (
-      <div key={key} style={style}>
-        <ReactDiffViewer
-          styles={newStyles}
-          oldValue={diffString.left[index]}
-          newValue={diffString.right[index]}
-          splitView={true}
-          compareMethod={DiffMethod.WORDS}
-          hideLineNumbers={true}
-          showDiffOnly={false}
-        />
-      </div>
-    );
+  };
+
+  for (let index = 0; index < 100; index++) {
+      rows.push(
+          <div key={index}>
+              <ReactDiffViewer
+                  styles={newStyles}
+                  oldValue={diffString.left[index]}
+                  newValue={diffString.right[index]}
+                  splitView={true}
+                  compareMethod={DiffMethod.WORDS}
+                  hideLineNumbers={true}
+                  showDiffOnly={false}
+              />
+          </div>
+      );
   }
 
   return (
-    <AutoSizer>
-      {({ width, height }) => (
-        <List
-          className={styles.List}
-          height={height}
-          rowCount={diffString.left.length}
-          rowHeight={getRowHeight}
-          rowRenderer={rowRenderer}
-          width={width}
-        />
-      )}
-    </AutoSizer>
+      <div className="tree-container">
+          {rows}
+      </div>
   )
 }
 
